13f2e25fb72aea973034de87dfc9b3a6c6653cff
Sirajudeen <ksiraj2002@gmail.com>
2022-01-27 14:00:55 +0530

    angular pwa

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+import { ScannerComponent } from "./scanner/scanner.component";
+import { RegistrationComponent } from "./registration/registration.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { NgxQRCodeModule } from "ngx-qrcode2";
+import { ZXingScannerModule } from "@zxing/ngx-scanner";
+import { ReactiveFormsModule } from "@angular/forms";
+@NgModule({
+  declarations: [
+    AppComponent,
+    ScannerComponent,
+    RegistrationComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production
+    }),
+    ReactiveFormsModule,
+    ZXingScannerModule,
+    NgxQRCodeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
