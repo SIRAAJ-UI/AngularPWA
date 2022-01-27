000a39a5478196681a93e4a759e5139c7135d12e
Sirajudeen <ksiraj2002@gmail.com>
2022-01-27 16:34:29 +0530

    latest change.

--- src/app/scanner/scanner.component.ts
+++ src/app/scanner/scanner.component.ts
@@ -15,18 +15,59 @@ export class ScannerComponent implements OnInit {
   hasPermission: boolean;
   qrResult: Guest;
   guestExist: boolean;
+  validQrCode: boolean;
   qrCode: string;
 
   constructor(private guestService: GuestService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // const qrString = "Student 160622,QB Id: 14909";
+    // // const qrString = "QB Id: 14909";
+    // this.onQrCodeValidationCheck(qrString);
+  }
 
   clearResult(): void {
     this.qrResult = null;
   }
 
+  onQrCodeValidationCheck(qrString){
+    try{
+      const firstSet = qrString.split(",");
+      let validResponse = [];
+      let props = ["StudentId","QuestionId"]
+      firstSet.forEach( (s,index) => {
+        const prop = props[index];
+        const value = s.split(":")[1];
+        if(value && value !== undefined){
+          let propValue:any = { };
+          propValue[prop] = String(value).trim();
+          validResponse.push(propValue)
+        } else {
+        }
+      })
+      console.log(validResponse)
+      if(validResponse.length === 2){
+        // Valid Qr Code!
+        this.validQrCode = true;
+        this.clearMessage();
+      } else {
+        console.log(validResponse)
+        // InValid Qr Code!
+        this.validQrCode = false;
+        this.clearMessage();
+
+      }
+    } catch {
+      this.validQrCode = false;
+      this.clearMessage();
+      // InValid Qr Code!
+    }
+    
+  }
   onCodeResult(resultString: string): void {
-    this.qrCode = resultString;
+    this.validQrCode = null;
+    this.onQrCodeValidationCheck(resultString);
+    // this.qrCode = resultString;
     // this.guestExist = null;
     // if (this.checkQRJSON(resultString)) {
     //   this.qrResult = JSON.parse(resultString);
@@ -42,27 +83,27 @@ export class ScannerComponent implements OnInit {
     this.hasPermission = has;
   }
 
-  checkInGuest(): void {
-    this.guestService.guests$
-      .pipe(
-        map(guests =>
-          guests.find((guest: Guest) => guest.id === this.qrResult.id)
-        )
-      )
-      .subscribe(guest => {
-        if (guest !== null && guest !== undefined) {
-          this.guestExist = true;
-        } else {
-          this.guestExist = false;
-        }
-        this.clearResult();
-        this.clearMessage();
-      });
-  }
+  // checkInGuest(): void {
+  //   this.guestService.guests$
+  //     .pipe(
+  //       map(guests =>
+  //         guests.find((guest: Guest) => guest.id === this.qrResult.id)
+  //       )
+  //     )
+  //     .subscribe(guest => {
+  //       if (guest !== null && guest !== undefined) {
+  //         this.guestExist = true;
+  //       } else {
+  //         this.guestExist = false;
+  //       }
+  //       this.clearResult();
+  //       this.clearMessage();
+  //     });
+  // }
 
   clearMessage() {
     setTimeout(() => {
-      this.guestExist = null;
+      this.validQrCode = null;
     }, 3000);
   }
 
